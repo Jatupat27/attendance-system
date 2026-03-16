e188b48b1897bb4dd9ae99096690c95549841616
Jatupat Anudachakul <151161007+Jatupat27@users.noreply.github.com>
2026-03-16 13:17:16 +0700

    update QR checkin with ngrok link

--- app/qr/page.js
+++ app/qr/page.js
@@ -4,15 +4,47 @@ import { QRCodeCanvas } from "qrcode.react";
 
 export default function QRPage() {
 
-  const checkinUrl = "https://attendance-system-mu-dun.vercel.app/checkin";
+  const checkinUrl = "https://obsequent-entangleable-paul.ngrok-free.dev/checkin";
 
   return (
-    <div style={{ padding: 20 }}>
-      <h1>QR Code เช็คชื่อ</h1>
+    <div style={{
+      padding:40,
+      textAlign:"center",
+      fontFamily:"sans-serif",
+      background:"#f3f4f6",
+      minHeight:"100vh"
+    }}>
 
-      <QRCodeCanvas value={checkinUrl} size={250} />
+      <h1 style={{
+        fontSize:32,
+        marginBottom:10
+      }}>
+        QR Code เช็คชื่อ
+      </h1>
 
-      <p>ให้นักศึกษาสแกน QR นี้เพื่อเช็คชื่อ</p>
+      <p style={{marginBottom:30}}>
+        ให้นักศึกษาสแกน QR นี้เพื่อเช็คชื่อ
+      </p>
+
+      <div style={{
+        background:"white",
+        display:"inline-block",
+        padding:30,
+        borderRadius:10,
+        boxShadow:"0 4px 10px rgba(0,0,0,0.1)"
+      }}>
+
+        <QRCodeCanvas
+          value={checkinUrl}
+          size={250}
+        />
+
+      </div>
+
+      <p style={{marginTop:20,color:"#666"}}>
+        {checkinUrl}
+      </p>
+
     </div>
   );
 }
